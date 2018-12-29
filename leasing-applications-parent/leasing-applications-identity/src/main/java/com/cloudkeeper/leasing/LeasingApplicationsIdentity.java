de54645cc560665aa34219e98c4781f4aef0c49d
hanfei888766 <1058763754@qq.com>
2018-12-29 19:27:49 +0800

    文件上传

--- leasing-applications-parent/leasing-applications-identity/src/main/java/com/cloudkeeper/leasing/LeasingApplicationsIdentity.java
+++ leasing-applications-parent/leasing-applications-identity/src/main/java/com/cloudkeeper/leasing/LeasingApplicationsIdentity.java
@@ -2,8 +2,12 @@ package com.cloudkeeper.leasing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -13,5 +17,10 @@ public class LeasingApplicationsIdentity {
     public static void main(String[] args) {
         SpringApplication.run(LeasingApplicationsIdentity.class, args);
     }
-
+    @Bean
+    public MultipartConfigElement multipartConfigElement(){
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("1024000KB");
+        return factory.createMultipartConfig();
+    }
 }
